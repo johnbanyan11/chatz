b9baf0a2b6ddbddc12c491bde4e66a167d19d394
johnbanyan11 <johnbanyan11@gmail.com>
2023-07-05 09:13:52 +0530

    google login setup

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,30 @@
+import { firebaseAuth } from "@/utils/FirebaseConfig";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import Image from "next/image";
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 
 function login() {
-  return <div>login</div>;
+  const handleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    const { user } = await signInWithPopup(firebaseAuth, provider);
+    console.log("userrr", user);
+  };
+  return (
+    <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-4">
+      <div className="flex flex-col items-center justify-center gap-2 text-white">
+        <Image src="/whatsapp.gif" alt="chatz" height={200} width={200} />
+        <span>ChatZZ</span>
+      </div>
+      <button
+        className="flex justify-center items-center gap-4 bg-search-input-container-background p-2 rounded-lg"
+        onClick={handleLogin}
+      >
+        <FcGoogle className="text-2xl" />
+        <span className="text-white text-xl">Login with Google</span>
+      </button>
+    </div>
+  );
 }
 
 export default login;
