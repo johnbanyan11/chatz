0f9995bd6f4d9f06a70c49f86e68ad597828664a
johnbanyan11 <johnbanyan11@gmail.com>
2023-07-07 07:56:34 +0530

    profile pic fix

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,9 +1,67 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FcCompactCamera } from "react-icons/fc";
+import ContextMenu from "./ContextMenu";
+import PhotoPicker from "./PhotoPicker";
 
 const Avatar = ({ type, image, setImage }) => {
   const [hover, setHover] = useState(false);
+  const [ismenuOpen, setIsMenuOpen] = useState(false);
+  const [menuCoords, setMenuCoords] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [grabpic, setGrabpic] = useState(false);
+
+  const openMenu = (e) => {
+    e.preventDefault();
+    setMenuCoords({ x: e.pageX, y: e.pageY });
+    setIsMenuOpen(true);
+  };
+
+  useEffect(() => {
+    if (grabpic) {
+      const data = document.getElementById("photo-picker");
+      data.click();
+      document.body.onfocus = (e) => {
+        setTimeout(() => {
+          setGrabpic(false);
+        }, 1000);
+      };
+    }
+  });
+
+  const menuOptions = [
+    { name: "Take Photo", callback: () => {} },
+    { name: "Choose from Library", callback: () => {} },
+    {
+      name: "Upload Photo",
+      callback: () => {
+        setGrabpic(true);
+      },
+    },
+    {
+      name: "Remove Photo",
+      callback: () => {
+        setImage("/default_avatar.png");
+      },
+    },
+  ];
+
+  const getPic = async (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    const data = document.createElement("img");
+    reader.onload = function (e) {
+      data.src = e.target.result;
+      data.setAttribute("data-src", e.target.result);
+    };
+    reader.readAsDataURL(file);
+    setTimeout(() => {
+      console.log(data.src);
+      setImage(data.src);
+    }, 100);
+  };
 
   return (
     <div className="flex items-center justify-center">
@@ -17,27 +75,47 @@ const Avatar = ({ type, image, setImage }) => {
           <Image src={image} alt="avatar" className="rounded-full" fill />
         </div>
       )}
-      {type === "xl" && (
+      {type === "xl" && image && (
         <div
           className="relative cursor-pointer z-0"
           onMouseEnter={() => setHover(true)}
           onMouseLeave={() => setHover(false)}
         >
           <div
+            id="context-opener"
             className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex flex-col gap-2 items-center justify-center ${
               hover ? "visible" : "hidden"
             }`}
+            onClick={(e) => openMenu(e)}
           >
-            <FcCompactCamera className="text-2xl" id="context-opener" />
-            <span>
+            <FcCompactCamera
+              id="context-opener"
+              className="text-2xl"
+              onClick={(e) => openMenu(e)}
+            />
+            <span id="context-opener" onClick={(e) => openMenu(e)}>
               Change <br /> Profile <br /> Photo
             </span>
           </div>
           <div className="flex items-center justify-center h-60 w-60 ">
-            <Image src={image} alt="avatar" className="rounded-full" fill />
+            <Image
+              src={image}
+              alt="avatar"
+              className="rounded-full object-contain"
+              fill
+            />
           </div>
         </div>
       )}
+      {ismenuOpen && (
+        <ContextMenu
+          options={menuOptions}
+          coords={menuCoords}
+          menu={ismenuOpen}
+          setMenu={setIsMenuOpen}
+        />
+      )}
+      {grabpic && <PhotoPicker onChange={getPic} />}
     </div>
   );
 };
--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-function ContextMenu() {
-  return <div>ContextMenu</div>;
+function ContextMenu({ options, coords, menu, setMenu }) {
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
+  const handleOutsideClick = (e) => {
+    if (e.target.id !== "context-opener") {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setMenu(false);
+      }
+    }
+  };
+
+  const handleClick = (e, callback) => {
+    e.stopPropagation();
+    setMenu(false);
+    callback();
+  };
+
+  return (
+    <div
+      style={{ top: coords.y, left: coords.x }}
+      className={`bg-dropdown-background fixed py-2 z-[100]`}
+      ref={menuRef}
+    >
+      <ul>
+        {options.map(({ name, callback }) => (
+          <li
+            key={name}
+            className="px-5 py-2 cursor-pointer hover:bg-background-default-hover"
+            onClick={(e) => handleClick(e, callback)}
+          >
+            <span className="text-white">{name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ContextMenu;
